0fbac5b029d5693dcb4e31b3c75a1512e1e588d8
Leo <hello@leo.glass>
2022-11-17 12:31:43 +0100

    Major rewrite

--- !RequiresLevelX/Core.lua
+++ !RequiresLevelX/Core.lua
@@ -14,35 +14,12 @@ frame:RegisterEvent("VARIABLES_LOADED")
 
 
 -- Function to add information to item tooltips
-local function OnTooltipSetItem(tooltip)
-	if not RequiresLevelXConfig["AddonEnabled"] then return end
+local function OnTooltipSetItem(tooltip, data)
+	-- Do nothing if the addon is disabled, or if there is no data
+	if not RequiresLevelXConfig["AddonEnabled"] or not data then return end
 
-	local tooltip = tooltip
-	local match = string.match
-	local _, link = tooltip:GetItem()
-	if not link then return end -- Break if the link is invalid
-	
-	-- String matching to get item ID
-	local itemString = match(link, "item[%-?%d:]+")
-	local _, itemId = strsplit(":", itemString or "")
-
-	-- TradeSkillFrame workaround
-	if itemId == "0" and TradeSkillFrame ~= nil and TradeSkillFrame:IsVisible() then
-		if (GetMouseFocus():GetName()) == "TradeSkillSkillIcon" then
-			itemId = GetTradeSkillItemLink(TradeSkillFrame.selectedSkill):match("item:(%d+):") or nil
-		else
-			for i = 1, 8 do
-				if (GetMouseFocus():GetName()) == "TradeSkillReagent"..i then
-					itemId = GetTradeSkillReagentItemLink(TradeSkillFrame.selectedSkill, i):match("item:(%d+):") or nil
-					break
-				end
-			end
-		end
-	end
-
-	itemId = tonumber(itemId) -- Make sure itemId is an integer
-
-	if not itemId then return end
+	local link = data.guid and C_Item.GetItemLinkByGUID(data.guid)
+	if not link then return end
 
 	local itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, itemSellPrice = GetItemInfo(link)
 
